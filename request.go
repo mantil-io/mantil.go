9ac7f55f859919f60102ee4dbc4115591aaa3981
Igor Anic <ianic@inet.hr>
2021-09-11 15:36:16 +0200

    refactor request type detection

---
+++ request.go
@@ -0,0 +1,159 @@
+package mantil
+
+import (
+	"encoding/json"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/mantil-io/mantil.go/pkg/proto"
+)
+
+type RequestType int
+
+const (
+	RequestTypeUnknown = iota
+	APIGateway
+	WSConnect
+	WSMessage
+	WSDisconnect
+	Streaming
+)
+
+type Request struct {
+	Type   RequestType
+	Method string
+	Body   []byte
+	Raw    []byte
+	attr   requestAttributes
+}
+
+type requestAttributes struct {
+	// API gateway and WebSocket attributes
+	Path           string            `json:"path"`
+	HTTPMethod     string            `json:"httpMethod"`
+	PathParameters map[string]string `json:"pathParameters"`
+	RequestContext struct {
+		Identity     map[string]interface{} `json:"identity"`
+		ConnectionID string                 `json:"connectionId"` // postoji samo kod ws
+		EventType    string                 `json:"eventType"`    // ws: MESSAGE,
+		Protocol     string                 `json:"protocol"`     // HTTP... // postoji samo kod API
+	} `json:"requestContext"`
+	Body string `json:"body"`
+
+	// streaming
+	ConnectionID string `json:"connectionID"`
+	Inbox        string `json:"inbox"`
+	URI          string `json:"uri"`
+	Payload      []byte `json:"payload"`
+
+	// for use in aws lmabda console to avoid putting quoted json into body
+	RawRequest json.RawMessage `json:"req"`
+}
+
+func parseRequest(raw []byte) (req Request) {
+	req = Request{
+		Type: RequestTypeUnknown,
+		Raw:  raw,
+	}
+	if err := json.Unmarshal(raw, &req.attr); err != nil {
+		req.Method = req.method()
+		req.Body = raw
+		if b := req.body(); b != nil {
+			req.Body = b
+		}
+		return
+	}
+	req.detectType()
+	req.Body = req.body()
+	req.Method = req.method()
+	return
+}
+
+func (r *Request) detectType() {
+	context := r.attr.RequestContext
+	if protocol := context.Protocol; protocol != "" && strings.HasPrefix(protocol, "HTTP") {
+		r.Type = APIGateway
+		return
+	}
+	if context.ConnectionID != "" {
+		switch context.EventType {
+		case "MESSAGE":
+			r.Type = WSMessage
+			return
+		case "CONNECT":
+			r.Type = WSConnect
+			return
+		case "DISCONNECT":
+			r.Type = WSDisconnect
+			return
+		default:
+			return
+		}
+	}
+	if r.attr.Path != "" && r.attr.HTTPMethod != "" {
+		r.Type = APIGateway
+		return
+	}
+	if r.attr.URI != "" && r.attr.ConnectionID != "" {
+		r.Type = Streaming
+		return
+	}
+}
+
+func (r *Request) method() string {
+	if r.Type == APIGateway {
+		return r.attr.PathParameters["path"]
+	}
+	if r.attr.URI != "" {
+		uriParts := strings.Split(r.attr.URI, ".")
+		if len(uriParts) >= 2 {
+			return uriParts[1]
+		}
+		return uriParts[0]
+	}
+	return ""
+}
+
+func (r *Request) body() []byte {
+	if len(r.attr.Body) > 0 {
+		return []byte(r.attr.Body)
+	}
+	if len(r.attr.Payload) > 0 {
+		return r.attr.Payload
+	}
+	if len(r.attr.RawRequest) > 0 {
+		return r.attr.RawRequest
+	}
+	if r.Type == RequestTypeUnknown {
+		return r.Raw
+	}
+	return nil
+}
+
+func (r *Request) AsAPIGatewayRequest() events.APIGatewayProxyRequest {
+	var agw events.APIGatewayProxyRequest
+	_ = json.Unmarshal(r.Raw, &agw)
+	return agw
+}
+
+func (r *Request) AsWebsocketRequest() events.APIGatewayWebsocketProxyRequest {
+	var wsr events.APIGatewayWebsocketProxyRequest
+	_ = json.Unmarshal(r.Raw, &wsr)
+	return wsr
+}
+
+func (r *Request) AsStreamingRequest() proto.Message {
+	var msg proto.Message
+	_ = json.Unmarshal(r.Raw, &msg)
+	return msg
+}
+
+func (r *Request) ToStreamingResponse(rspPayload []byte) proto.Message {
+	return proto.Message{
+		Type:         proto.Response,
+		ConnectionID: r.attr.ConnectionID,
+		URI:          r.attr.URI,
+		Inbox:        r.attr.Inbox,
+		Payload:      rspPayload,
+	}
+}
--- request_test.go
+++ request_test.go
@@ -7,20 +7,17 @@ import (
 	"io/ioutil"
 	"math/rand"
 	"strconv"
-	"strings"
 	"testing"
 	"time"
 
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/mantil-io/mantil.go/pkg/proto"
 	"github.com/stretchr/testify/require"
 )
 
-func TestDetectPayloadType(t *testing.T) {
+func TestParseRequest(t *testing.T) {
 	data := []struct {
-		filename    string
-		payloadType RequestType
-		method      string
+		filename string
+		typ      RequestType
+		method   string
 	}{
 		{"api-gateway.json", APIGateway, "MethodName"},
 		{"ws-connect.json", WSConnect, ""},
@@ -31,150 +28,28 @@ func TestDetectPayloadType(t *testing.T) {
 		{"bad_attribute_type.json", RequestTypeUnknown, "method"},
 	}
 
-	for i, d := range data {
+	for _, d := range data {
 		buf, err := ioutil.ReadFile("testdata/" + d.filename)
 		require.NoError(t, err)
-		py := detectPayloadType(buf)
-		require.Equal(t, d.payloadType, py.Type, "request type detect failed for %d", i)
-		require.Equal(t, buf, py.Raw)
-		require.Equal(t, d.method, py.Method(), d.filename)
-		if py.Type == WSConnect || py.Type == WSDisconnect {
-			continue
-		}
-		if py.Type == Streaming {
+		req := parseRequest(buf)
+		require.Equal(t, d.typ, req.Type, d.filename)
+		require.Equal(t, buf, req.Raw)
+		require.Equal(t, d.method, req.Method, d.filename)
+		if req.Type == WSConnect || req.Type == WSDisconnect {
+			require.Nil(t, req.Body)
 			continue
 		}
 		var kv struct {
 			Key string
 		}
-		//fmt.Printf("going to unmarshal body: `%s`\n%v\n", py.Body(), py.Body())
-		err = json.Unmarshal(py.Body(), &kv)
+		err = json.Unmarshal(req.Body, &kv)
 		require.NoError(t, err, d.filename)
-		require.Equal(t, "value", kv.Key, "case %d", i)
+		require.Equal(t, "value", kv.Key, d.filename)
 	}
-}
-
-type RequestType int
-
-const (
-	RequestTypeUnknown = iota
-	APIGateway
-	WSConnect
-	WSMessage
-	WSDisconnect
-	Streaming
-)
-
-type Payload struct {
-	Type  RequestType
-	Raw   []byte
-	probe typeProbe
-}
-
-func (p *Payload) Method() string {
-	if p.Type == APIGateway {
-		return p.probe.PathParameters["path"]
-	}
-	if p.probe.URI != "" {
-		uriParts := strings.Split(p.probe.URI, ".")
-		if len(uriParts) >= 2 {
-			return uriParts[1]
-		}
-		return uriParts[0]
-	}
-	return ""
-}
-
-func (p *Payload) Body() []byte {
-	if len(p.probe.Body) > 0 {
-		return unquoteJSON(p.probe.Body)
-	}
-	if len(p.probe.Payload) > 0 {
-		return p.probe.Payload
-	}
-	return nil
-}
-
-func detectPayloadType(raw []byte) (payload Payload) {
-	payload = Payload{
-		Type: RequestTypeUnknown,
-		Raw:  raw,
-	}
-	if err := json.Unmarshal(raw, &payload.probe); err != nil {
-		return
-	}
-	context := payload.probe.RequestContext
-	if protocol := context.Protocol; protocol != "" && strings.HasPrefix(protocol, "HTTP") {
-		payload.Type = APIGateway
-		return
-	}
-	if context.ConnectionID != "" {
-		switch context.EventType {
-		case "MESSAGE":
-			payload.Type = WSMessage
-			return
-		case "CONNECT":
-			payload.Type = WSConnect
-			return
-		case "DISCONNECT":
-			payload.Type = WSDisconnect
-			return
-		default:
-			return
-		}
-	}
-	if payload.probe.Path != "" && payload.probe.HTTPMethod != "" {
-		payload.Type = APIGateway
-		return
-	}
-	if payload.probe.URI != "" && payload.probe.ConnectionID != "" {
-		payload.Type = Streaming
-		return
-	}
-	return
-}
-
-type typeProbe struct {
-	Path           string            `json:"path"`
-	HTTPMethod     string            `json:"httpMethod"`
-	PathParameters map[string]string `json:"pathParameters"`
-	RequestContext struct {
-		Identity     map[string]interface{} `json:"identity"`
-		ConnectionID string                 `json:"connectionId"` // postoji samo kod ws
-		EventType    string                 `json:"eventType"`    // ws: MESSAGE,
-		Protocol     string                 `json:"protocol"`     // HTTP... // postoji samo kod API
-	} `json:"requestContext"`
-	Body json.RawMessage `json:"body"`
-
-	// streaming
-	ConnectionID string `json:"connectionID"`
-	Inbox        string `json:"inbox"`
-	URI          string `json:"uri"`
-	Payload      []byte `json:"payload"`
-
-	// for use in aws lmabda console to avoid putting quoted json into body
-	RawRequest json.RawMessage `json:"req"`
-}
-
-func (p *Payload) AsAPIGatewayRequest() events.APIGatewayProxyRequest {
-	var req events.APIGatewayProxyRequest
-	_ = json.Unmarshal(p.Raw, &req)
-	return req
-}
-
-func (p *Payload) AsWebsocketRequest() events.APIGatewayWebsocketProxyRequest {
-	var req events.APIGatewayWebsocketProxyRequest
-	_ = json.Unmarshal(p.Raw, &req)
-	return req
-}
-
-func (p *Payload) AsStreamingRequest() proto.Message {
-	var m proto.Message
-	_ = json.Unmarshal(p.Raw, &m)
-	return m
 }
 
 func TestNoDoubleEncoding(t *testing.T) {
+	t.Skip()
 	json1 := `{"body": {"key": "value"}}`
 	var s struct {
 		Body json.RawMessage
@@ -251,6 +126,7 @@ func unquoteJSON(raw []byte) []byte {
 }
 
 func TestDouble(t *testing.T) {
+	t.Skip()
 	st := struct {
 		Key string
 	}{Key: "value"}
@@ -328,9 +204,9 @@ func BenchmarkString(b *testing.B) {
 	//	fmt.Printf("BenchmarkString buf: %s\n", buf)
 
 	for n := 0; n < b.N; n++ {
-		req := detectPayloadType(buf)
+		req := parseRequest(buf)
 		var a A
-		err := json.Unmarshal(req.Body(), &a)
+		err := json.Unmarshal(req.Body, &a)
 		if err != nil {
 			b.Fail()
 		}
@@ -350,9 +226,9 @@ func BenchmarkBytes(b *testing.B) {
 	//fmt.Printf("BenchmarkBytes buf: %s\n", buf)
 
 	for n := 0; n < b.N; n++ {
-		req := detectPayloadType(buf)
+		req := parseRequest(buf)
 		var a A
-		err := json.Unmarshal(req.Body(), &a)
+		err := json.Unmarshal(req.Body, &a)
 		if err != nil {
 			b.Fail()
 		}
@@ -370,9 +246,9 @@ func BenchmarkInner(b *testing.B) {
 	//fmt.Printf("BenchmarkInner buf: %s\n", buf)
 
 	for n := 0; n < b.N; n++ {
-		req := detectPayloadType(buf)
+		req := parseRequest(buf)
 		var a A
-		err := json.Unmarshal(req.Body(), &a)
+		err := json.Unmarshal(req.Body, &a)
 		if err != nil {
 			b.Fail()
 		}
@@ -392,9 +268,9 @@ func BenchmarkStringArray(b *testing.B) {
 	//fmt.Printf("BenchmarkString buf: %s\n", buf)
 
 	for n := 0; n < b.N; n++ {
-		req := detectPayloadType(buf)
+		req := parseRequest(buf)
 		var a []A
-		err := json.Unmarshal(req.Body(), &a)
+		err := json.Unmarshal(req.Body, &a)
 		if err != nil {
 			fmt.Printf("error: %s\n", err)
 			b.Fail()
@@ -415,9 +291,9 @@ func BenchmarkBytesArray(b *testing.B) {
 	//fmt.Printf("BenchmarkBytes buf: %s\n", buf)
 
 	for n := 0; n < b.N; n++ {
-		req := detectPayloadType(buf)
+		req := parseRequest(buf)
 		var a []A
-		err := json.Unmarshal(req.Body(), &a)
+		err := json.Unmarshal(req.Body, &a)
 		if err != nil {
 			b.Fail()
 		}
@@ -435,9 +311,9 @@ func BenchmarkInnerArray(b *testing.B) {
 	//fmt.Printf("BenchmarkInner buf: %s\n", buf)
 
 	for n := 0; n < b.N; n++ {
-		req := detectPayloadType(buf)
+		req := parseRequest(buf)
 		var a []A
-		err := json.Unmarshal(req.Body(), &a)
+		err := json.Unmarshal(req.Body, &a)
 		if err != nil {
 			b.Fail()
 		}
